32035e18b7179ce3294baf177cb1a31954827636
Samuel Adeshina <samueladeshina73@gmail.com>
2015-09-02 20:30:34 +0100

    Java Implementation of the Insertion Sort Algorithm

--- insertion_sort.java
+++ insertion_sort.java
@@ -1,13 +1,18 @@
+import java.util.Scanner;
 class insertion_sort
 {
 	private int[] input;
+
+	/* ----- CONSTRUCTOR ------ */
 	public insertion_sort() { }
 
+	/* ----- ACCEPTS AN ARRAY OF VALUE TO SORT ---- */
 	public void setInput(int[] arr)
 	{
 		this.input = arr;
 	}
 
+	/* ---- SORTS AN ARRAY USING THE INSERTION SORT ALGORITHM --- */
 	public void sort()
 	{
 		int lengthOfInput = this.input.length;
@@ -28,30 +33,42 @@ class insertion_sort
 		}
 	}
 
+
+	/* ---- RETURNS THE SORTED ARRAY ---- */
 	public void getInput()
 	{
 		for (int i:this.input)
 		{
-			System.out.println(i);
+			System.out.print(i+" \t");
 		}
 	}
 
+
+	/* ----- A METHOD FOR ACCEPTING USER DATA ---- */
+	protected int[] accept()
+	{
+		Scanner scannerObj = new Scanner(System.in);
+		System.out.print("How many items do you want to sort: ");
+		int length = scannerObj.nextInt();
+		int[] input = new int[length];
+		for (int i=1; i <= length; i++)
+		{
+			System.out.print("Enter The "+ i+" number: \t");
+			input[i - 1] = scannerObj.nextInt();
+		}
+
+		return input;
+	}
+
+
+	/* --- APPLICATION ENTRY POINT ----- */
 	public static void main(String[] args)
 	{
 		insertion_sort isObj = new insertion_sort();
-		int[] input = {12, 42, 56, 11, 245, 1};
+		int[] input = isObj.accept();
 		isObj.setInput(input);
 		isObj.sort();
-		isObj.getInput();
+		System.out.print("\n\nResult: "); isObj.getInput();
 	}
 
 }
-
-
-/*
-{1, 3, 4, 5, 6}
-
-input[0] > input[1] && 
-
-
-*/
